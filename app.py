5cedc5cbfc9881c03c5aca9d300c842a07c3fa37
ZnPdCo <poiuytreertyuiop@outlook.com>
2024-11-11 17:00:45 +0800

    拆分

--- app.py
+++ app.py
@@ -8,61 +8,14 @@ import numpy as np
 from database import connect_db
 from utils import check_login, check_admin, get_username, random_string
 from view.routes import view
+from auth.routes import auth
 
 app = Flask(__name__)
 app.config['TITLE'] = 'Rating System'
 
 app.register_blueprint(view, url_prefix='/')
+app.register_blueprint(auth, url_prefix='/')
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html', title=current_app.config['TITLE'], logged_in=check_login(request.cookies.get('id')), is_admin=check_admin(request.cookies.get('id')))
-
-@app.route('/login/', methods=['GET', 'POST'])
-def login():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    if username is None and password is None:
-        return render_template('login.html', title=current_app.config['TITLE'], logged_in=check_login(request.cookies.get('id')), is_admin=check_admin(request.cookies.get('id')))
-    else:
-        if len(username) == 0 or len(username) > 100 or len(password) == 0 or len(password) > 100:
-            return "<script>location.search='?error=3';</script>"
-
-        # check user exist
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM users WHERE username=?", (username, ))
-        user = cursor.fetchone()
-        if user is None:
-            if False:
-                return "<script>location.search='?error=2';</script>"
-            # register user
-            cursor.execute("INSERT INTO users (id, username, password) VALUES (?,?,?)", (random_string(128), username, hashlib.sha256(password.encode('utf-8')).hexdigest()))
-            conn.commit()
-        conn.close()
-
-        # check password
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id, password FROM users WHERE username=?", (username, ))
-        user = cursor.fetchone()
-        if hashlib.sha256(password.encode('utf-8')).hexdigest() != user[1]:
-            return "<script>location.search='?error=1';</script>"
-        conn.close()
-
-        # if the first username, it is admin
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute("SELECT id FROM users")
-        res = cursor.fetchall()
-        if len(res) == 1:
-            cursor.execute("UPDATE users SET admin=1 WHERE id=?", (user[0], ))
-            conn.commit()
-        conn.close()
-
-        resp = make_response("<script>location.href='/';</script>")
-        resp.set_cookie('id', user[0], httponly=False, max_age=3600*24*30)
-        return resp
 
 @app.route('/admin/', methods=['GET'])
 def admin():
@@ -390,9 +343,3 @@ def report():
 
     return ""
     
-
-@app.route('/logout/', methods=['GET'])
-def logout():
-    resp = make_response("<script>location.href='/';</script>")
-    resp.set_cookie('id', '', expires=0)
-    return resp
--- view/routes.py
+++ view/routes.py
@@ -3,6 +3,10 @@ from utils import check_login, check_admin
 
 view = Blueprint('view', __name__)
 
+@view.route('/', methods=['GET'])
+def index():
+    return render_template('index.html', title=current_app.config['TITLE'], logged_in=check_login(request.cookies.get('id')), is_admin=check_admin(request.cookies.get('id')))
+
 @view.route('/legal/', methods=['GET'])
 def legal():
     return render_template('legal.html', title=current_app.config['TITLE'], logged_in=check_login(request.cookies.get('id')), is_admin=check_admin(request.cookies.get('id')))
