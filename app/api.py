843af05c2c772a3355ca0452620345d5460a4fe0
ZnPdCo <poiuytreertyuiop@outlook.com>
2024-11-14 20:41:25 +0800

    fix same pid in #3

--- app/api.py
+++ app/api.py
@@ -13,6 +13,18 @@ def add_problem(contest, name, info):
     """
     conn = connect_db()
     cursor = conn.cursor()
+    # have same pid
+    if "pid" in info:
+        cursor.execute("SELECT contest FROM problems WHERE OJpid =?", (info["pid"],))
+        res = cursor.fetchone()
+        if res is not None:
+            cursor.execute(
+                "UPDATE problems SET contest = ? WHERE OJpid = ?",
+                (res[0] + "," + contest + "(" + name + ")", info["pid"]),
+            )
+            conn.commit()
+            conn.close()
+            return
     cursor.execute(
         "INSERT INTO problems (OJpid, contest, name, info) VALUES (?,?,?,?)",
         (
