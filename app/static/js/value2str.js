6ecf89aa5da58f26765e483643e18321aa385dc6
ZnPdCo <poiuytreertyuiop@outlook.com>
2024-11-16 23:54:22 +0800

    fix #11 difficulty circle

--- app/static/js/value2str.js
+++ app/static/js/value2str.js
@@ -14,43 +14,57 @@ function name2Str(pid, name, func) {
 }
 
 function difficulty2Str(difficulty, cnt) {
-  var res = '<td style="';
-  if (difficulty >= 2400) {
-    res += 'color: red;';
+  var res = $('<td>')
+  if(difficulty == null) {
+    res.css('color', 'gray');
+  } else if (difficulty >= 2400) {
+    res.css('color', 'red');
   } else if (difficulty >= 2100) {
-    res += 'color: rgb(255,140,0);';
-  } else if (difficulty >= 0) {
-    res += 'color: rgb(170,0,170);';
+    res.css('color', 'rgb(255,140,0)');
+  } else if (difficulty >= 1900) {
+    res.css('color', 'rgb(170,0,170)');
   } else if (difficulty >= 1600) {
-    res += 'color: blue;';
+    res.css('color', 'blue');
   } else if (difficulty >= 1400) {
-    res += 'color: rgb(3,168,158);';
+    res.css('color', 'rgb(3,168,158)');
   } else if (difficulty >= 1200) {
-    res += 'color: green;';
+    res.css('color', 'green');
   } else if (difficulty >= 1000) {
-    res += 'color: rgb(136,204,34);';
+    res.css('color', 'rgb(136,204,34)');
   } else {
-    res += 'color: gray;';
+    res.css('color', 'gray');
   }
-  res +=
-    `font-weight: 500;" data-toggle="popover" ` +
-    (cnt == null ? '' : `data-tooltip="Number of votes: ${cnt}"`) +
-    ` data-original-title="" title=""> ${difficulty2Circle(difficulty)}${difficulty == null ? 'N/A' : Math.round(difficulty)}</td>`;
+  res.css('font-weight', '500')
+  res.attr('data-toggle', 'popover')
+  if(cnt != null) res.attr('data-tooltip', `Number of votes: ${cnt}`)
+  res.append(difficulty2Circle(difficulty));
+  res.append(` ${difficulty == null ? 'N/A' : Math.round(difficulty)}`);
   return res;
 }
 
 function difficulty2Circle(difficulty) {
   var percentage = 0;
   var col = '';
-  if (difficulty >= 2400) {
+  var res = $('<span>');
+  res.css({'display': 'inline-block', 
+              'border-radius': '50%', 
+              'border-style':'solid',
+              'border-width': '1px',
+             'margin-right': '5px',
+              'height': '12px',
+              'width': '12px'});
+  if(difficulty == null) {
+    col += 'gray';
+    percentage = 0;
+  } else if (difficulty >= 2400 && difficulty < 3000) {
     col += 'red';
-    percentage = (difficulty - 2400) / 9;
+    percentage = (difficulty - 2400) / 6;
   } else if (difficulty >= 2100) {
     col += 'rgb(255,140,0)';
     percentage = (difficulty - 2100) / 3;
-  } else if (difficulty >= 0) {
+  } else if (difficulty >= 1900) {
     col += 'rgb(170,0,170)';
-    percentage = (difficulty - 0) / 2;
+    percentage = (difficulty - 1900) / 2;
   } else if (difficulty >= 1600) {
     col += 'blue';
     percentage = (difficulty - 1600) / 3;
@@ -65,44 +79,52 @@ function difficulty2Circle(difficulty) {
     percentage = (difficulty - 1000) / 2;
   } else {
     col += 'gray';
-    percentage = difficulty / 10;
+    percentage = (difficulty - 800) / 2;
   }
-  percentage = Math.round(10 * percentage) / 10;
-  var res = `<span style="display: inline-block; 
-                            border-radius: 50%; 
-                            border-style: solid;
-                            border-width: 1px;
-                            margin-right: 5px;
-                            height: 12px;
-                            width: 12px;
-                            border-color: ${col};
-                            background: linear-gradient(to top, ${col} 0%, ${col} ${percentage}%, rgba(0, 0, 0, 0) ${percentage}%, rgba(0, 0, 0, 0) 100%);"></span>`;
+  if(difficulty >= 3400) {
+    res.css({'border-color': '#FFD700',
+    'background': 'linear-gradient(to right, #FFD700, white, #FFD700)'});
+  } else if(difficulty >= 3200) {
+    res.css({'border-color': '#808080',
+    'background': 'linear-gradient(to right, #808080, white, #808080)'});
+} else if(difficulty >= 3000) {
+  res.css({'border-color': '#965C2C',
+  'background': 'linear-gradient(to right, #965C2C, #FFDABD, #965C2C)'});
+}else {
+    percentage = Math.round(10 * percentage) / 10;
+    res.css({
+              'border-color': col,
+              'background': `linear-gradient(to top, ${col} 0%, ${col} ${percentage}%, rgba(0, 0, 0, 0) ${percentage}%, rgba(0, 0, 0, 0) 100%)`})
+  }
   return res;
 }
 
 function quality2Str(quality, cnt) {
   var showQuality = Math.round(quality * 10) / 10;
-  var res = '<td style="';
+  var res = $('<td>')
   if (quality == null) {
-    res += 'color: gray;';
+    res.css('color', 'gray');
     showQuality = 'N/A';
   } else {
     quality = Math.round(quality);
-    if (quality == 1) {
-      res += 'color: gray;';
+    if(quality == 0) {
+      res.css('color', 'rgb(157, 108, 73)');
+      showQuality = '💩 ' + showQuality;
+    } else if (quality == 1) {
+      res.css('color', 'gray');
     } else if (quality == 2) {
-      res += 'color: rgb(144, 238, 144);';
+      res.css('color', 'rgb(144, 238, 144)');
     } else if (quality == 3) {
-      res += 'color: rgb(80, 200, 120);';
+      res.css('color', 'rgb(80, 200, 120)');
     } else if (quality == 4) {
-      res += 'color: rgb(34, 139, 34);';
+      res.css('color', 'rgb(34, 139, 34)');
     } else {
-      res += 'color: rgb(0, 128, 0);';
+      res.css('color', 'rgb(0, 128, 0)')
     }
   }
-  res +=
-    `font-weight: 500;" data-toggle="popover" ` +
-    (cnt == null ? '' : `data-tooltip="Number of votes: ${cnt}"`) +
-    ` data-original-title="" title="">${showQuality}</td>`;
+  res.css('font-weight', '500')
+  res.attr('data-toggle', 'popover')
+  if(cnt != null) res.attr('data-tooltip', `Number of votes: ${cnt}`)
+  res.text(showQuality);
   return res;
 }
